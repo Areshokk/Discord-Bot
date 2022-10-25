a407d598c6d8ae81beb79872257fea425358d0c0
Yura <yrameda0404@gmail.com>
2022-10-25 17:16:08 +0300

    update files

--- src/main/java/musicquizbot/Constants.java
+++
@@ -1,5 +0,0 @@
-package musicquizbot;
-
-public class Constants {
-	public static String JDA_TOKEN = "MTAwNjY4NTg0MTczMDU4ODc3Ng.GIaA29._BB6zRP_Jt_UE4LNyEQP5kjmi4zpCfsarbB88c";
-}
--- src/main/java/musicquizbot/commands/types/PrivateCommand.java
+++
@@ -1,9 +0,0 @@
-package musicquizbot.commands.types;
-
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.PrivateChannel;
-import net.dv8tion.jda.api.entities.User;
-
-public interface PrivateCommand {
-	public void performCommand(User u, PrivateChannel channel, Message message);
-}
